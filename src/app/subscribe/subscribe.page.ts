09371f54dd64235ce505da13accd6f39f8229520
Joseph <josephskhella@gmail.com>
2025-04-09 17:53:40 +0200

    subscribe page init

---
+++ src/app/subscribe/subscribe.page.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-subscribe',
+  templateUrl: './subscribe.page.html',
+  styleUrls: ['./subscribe.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class SubscribePage implements OnInit {
+  formData = {
+    name: '',
+    email: '',
+    childName: '',
+    birthDate: '',
+    birthMonth: '',
+    birthYear: '',
+    grade: '',
+    gender: '',
+    selectedTopics: [] as string[]
+  };
+
+  days = Array.from({length: 31}, (_, i) => i + 1);
+  months = ['January', 'February', 'March', 'April', 'May', 'June', 
+           'July', 'August', 'September', 'October', 'November', 'December'];
+  years = Array.from({length: 20}, (_, i) => new Date().getFullYear() - i);
+  grades = Array.from({length: 12}, (_, i) => i + 1);
+  topics = ['Arabic', 'Islamic', 'English', 'History', 'Sports'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  selectGender(gender: string) {
+    this.formData.gender = gender;
+  }
+
+  isTopicSelected(topic: string): boolean {
+    return this.formData.selectedTopics.includes(topic);
+  }
+
+  toggleTopic(topic: string) {
+    const index = this.formData.selectedTopics.indexOf(topic);
+    if (index === -1) {
+      if (this.formData.selectedTopics.length < 3) {
+        this.formData.selectedTopics.push(topic);
+      }
+    } else {
+      this.formData.selectedTopics.splice(index, 1);
+    }
+  }
+
+  onSubmit() {
+    console.log('Form submitted:', this.formData);
+  }
+} 
